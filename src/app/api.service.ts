0dd302aa14f47c766378a56a1a1e243c98f19524
mainak dalui <dalui.mainak@gmail.com>
2022-09-20 21:35:52 +0530

    initial commit

---
+++ src/app/api.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Bid } from './model/bid.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private url: string;
+
+  constructor(private http: HttpClient) { 
+    this.url='http://localhost:8081/e-auction/api/v1/seller';
+  }
+
+  getAllBidsByProductId(productId: string){
+    console.log("calling service for prod-id: ", productId);
+    return this.http.get<Bid[]>(`${this.url}/show-bids/${productId}`);
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
+import { Bid } from './model/bid.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'eauction-ui';
+  title = 'eauction';
+  productId: string;
+  bidList = Array<Bid>();
+
+  columnHeader: string[] = ['startingPrice', 'shortDesc', 'detailDesc', 'category', 'bidEndDate'];
+
+  constructor(private api: ApiService) {
+    this.productId = '';
+    this.bidList = [];
+  }
+
+  getBidList() {
+    console.log('for product id - ', this.productId);
+    this.bidList = [];
+    // call service to make api call to get list of bid
+    this.api.getAllBidsByProductId(this.productId).subscribe((
+      data) => {
+      this.bidList = data;
+      console.log('bid list - ', this.bidList);
+    }, err => {
+      console.log(`error occurred while fetching bid for product ${err}`);
+      console.log('error details: ', err);
+      alert('error occurred, see console log for error details');
+    });
+  }
+
 }
