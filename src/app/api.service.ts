3208a0ba0fada021a9a47654133680f00c82c321
mainakdalui <dalui.mainak@gmail.com>
2022-11-17 17:57:49 +0530

    minor url change

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -10,7 +10,7 @@ export class ApiService {
   private url: string;
 
   constructor(private http: HttpClient) { 
-    this.url='https://13yslr0gji.execute-api.ap-northeast-1.amazonaws.com/e-auction/api/v1/seller/';
+    this.url='https://13yslr0gji.execute-api.ap-northeast-1.amazonaws.com/e-auction/api/v1/seller';
   }
 
   getAllBidsByProductId(productId: string){
